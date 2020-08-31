c0adf9f6e300ce3b25c66a30f68b6b5d2d72c592
Vladimir Sterlin <vovasterlin@yahoo.com>
2020-08-31 12:52:18 -0700

    added generics fix

--- src/Stack/Test.java
+++ src/Stack/Test.java
@@ -3,7 +3,7 @@ package Stack;
 public class Test {
 
 	public static void main(String[] args) {
-		Stack stack = new Stack();
+		Stack<Integer> stack = new Stack<Integer>();
 		
 		for(int i=0;i<5;i++) {
 			stack.push(i);
